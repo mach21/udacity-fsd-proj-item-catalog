716ce53da7bb7fe948b5961ece972c0ff450554d
Vlad Chiriacescu <vladc@outlook.com>
2019-04-13 21:38:50 -0500

    rework the database schema

--- vagrant/catalog/db_setup.py
+++ vagrant/catalog/db_setup.py
@@ -17,27 +17,27 @@ class User(Base):
 class Team(Base):
     __tablename__ = 'team'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False, unique=True)
+    nickname = Column(String(25), nullable=False, unique=True)
 
     @property
     def serialize(self):
         """Return object data in easily serializeable format."""
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'nickname': self.nickname
         }
 
 
 class Player(Base):
     __tablename__ = 'player'
 
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-    position = Column(String(250), nullable=False)
-    jersey_number = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    jersey_number = Column(Integer, nullable=False, unique=True)
+    position = Column(String(15), nullable=False)
     team_id = Column(Integer, ForeignKey('team.id'))
     team = relationship(Team)
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -49,8 +49,8 @@ class Player(Base):
         return {
             'id': self.id,
             'name': self.name,
-            'position': self.position,
-            'jersey_number': self.jersey_number
+            'jersey_number': self.jersey_number,
+            'position': self.position
         }
 
 
